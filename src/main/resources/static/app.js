30e24ec57c29cced378857c4bfc5415c861b7068
Juan Camilo Herrera <juan.herrera@mail.escuelaing.edu.co>
2017-10-04 16:25:36 -0500

    PARTE 1.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,9 +36,8 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                alert("punto x: "+JSON.parse(eventbody.body).x+", punto y: "+JSON.parse(eventbody.body).y);
             });
         });
 
@@ -59,7 +58,7 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
-
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt)); 
             //publicar el evento
         },
 
